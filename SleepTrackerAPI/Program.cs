4c60f88185008343555673cc235c3d220183611e
Aishyuu <ivanmatias1331@gmail.com>
2024-08-12 20:59:01 -0700

    Added Migration and Updated Database

--- SleepTrackerAPI/Program.cs
+++ SleepTrackerAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
