6d8e71f475b6a596ed701eccc84329e001218e5a
Aishyuu <ivanmatias1331@gmail.com>
2024-08-12 20:05:45 -0700

    Added SleepData Model Type for Database

--- SleepTrackerAPI/Model/SleepData.cs
+++ SleepTrackerAPI/Model/SleepData.cs
@@ -2,5 +2,8 @@
 
 public class SleepData
 {
-    
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
 }
